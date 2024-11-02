be0401504e34a314114772b23cce25c34ff03d16
JeremyAS-py <jeremysoteloavalos@gmail.com>
2024-11-02 03:00:33 -0500

    Choleski.py
    Proyecto álgebra lineal

---
+++ Choleski.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def cholesky_decomposition(A):
+    n = A.shape[0]
+    L = np.zeros_like(A)  # Inicializa L como matriz compuesta por ceros
+
+    for i in range(n):
+        for j in range(i + 1):
+            sum_k = sum(L[i][k] * L[j][k] for k in range(j))  # Suma parcial
+            if i == j:
+                L[i][j] = np.sqrt(A[i][i] - sum_k)  # Elementos de la diagonal de L 
+            else:
+                L[i][j] = (A[i][j] - sum_k) / L[j][j]  # Elementos que no pertenecen a la diagonal
+    return L
+
+def regression_cholesky(X, y):
+    XtX = X.T @ X  # Matriz de correlación de X
+    Xty = X.T @ y  # Producto de X transpuesta y y
+    L = cholesky_decomposition(XtX)  # Descomposición de Cholesky de XtX
+    z = np.linalg.solve(L, Xty)  # Resuelve Lz = Xty
+    beta = np.linalg.solve(L.T, z)  # Resuelve L.T * beta = z
+    return beta
